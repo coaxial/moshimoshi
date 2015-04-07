75e476f1897e812d645208102f4429d28db68c4f
Pierre-Yves Poujade <py@poujade.org>
2015-04-06 22:01:07 -0400

    Use dummy caller id

---
+++ app/call_controllers/outbound_call.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+class OutboundCall < Adhearsion::CallController
+
+  def run
+    answer
+    extract_dialled_number
+    logger.info "Calling #{@dialled_number}..."
+    # TODO
+    # Handle caller id to reflect the real caller id
+    dial "sofia/gateway/cwu/#{@dialled_number}", from: 'Test <15554564567>', ringback: "file:///usr/share/assets/us_ringback_tone.wav" # The path to the file is the path on the FreeSWITCH machine. Only WAV files seem to play.
+    logger.info "Call to #{@dialled_number} ended."
+    say 'The other party hung up. Goodbye.'
+    hangup
+  end
+
+  def extract_dialled_number
+    number = call.to.match(/(\d*)@.*/)[1]
+    @dialled_number = number.empty? ? 'no digits' : number
+  end
+end
