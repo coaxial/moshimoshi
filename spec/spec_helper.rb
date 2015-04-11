7e0cee6837a99b1a71924516cb0aa497a4ef57e0
Pierre-Yves Poujade <py@poujade.org>
2015-04-11 17:56:22 -0400

    Update deprecated require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 ENV["AHN_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'adhearsion/rspec'
-require 'rspec/autorun'
+require 'rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
