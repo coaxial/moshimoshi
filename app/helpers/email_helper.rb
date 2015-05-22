d88a12702d1be86907e3f441062cbb6053ac3c44
Pierre-Yves Poujade <py@poujade.org>
2015-05-22 16:57:24 -0400

    Delete recordings after 9 months

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -12,6 +12,7 @@ module EmailHelper
     # Making these variables accessible to the haml template
     @public_url = public_url
     @recording_metadata = recording_metadata
+    @recording_expiry_date = recording_expiry_date
     send_recording_email_html = Tilt::HamlTemplate.new('templates/send_recording.haml').render(self)
     self.init
     @client.deliver(
@@ -20,5 +21,9 @@ module EmailHelper
       subject: "Your call to #{recording_metadata[:to]}",
       html_body: send_recording_email_html
     )
+  end
+
+  def self.recording_expiry_date
+    Time.now + 60 * 60 * 24 * 270
   end
 end
