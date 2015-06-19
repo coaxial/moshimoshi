0813ec68a2d4dc53ab2a9f3f53b447edf78efa7a
Pierre-Yves Poujade <py@poujade.org>
2015-06-19 18:31:39 -0400

    Remove redundant `bundle install` (now in Dockerfile's ONBUILD for Adhearsion)

--- lib/ahn/start.sh
+++ lib/ahn/start.sh
@@ -3,9 +3,6 @@ printf "Exporting env vars...\n"
 source /usr/src/app/lib/ahn/.env
 export $(cut -d= -f1 /usr/src/app/lib/ahn/.env)
 printf "Done.\n"
-printf "Installing gems...\n"
-cd /usr/src/app && bundle install
-printf "Done.\n"
 printf "Starting ahn...\n"
 ahn start /usr/src/app
 printf "Done.\n"
